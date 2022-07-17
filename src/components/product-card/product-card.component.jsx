a0aea990f86b2578cf934a534127d657e8cc54d6
Amil Silahic <jinobie02@gmail.com>
2022-07-16 18:53:01 -0600

    Updated Button Component & Styling Products/SignIn

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,14 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
-import Button from "../button/button.component";
-import "./product-card.styles.scss";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+
+import {
+  ProductCardContainer,
+  ProductFooter,
+  Name,
+  Price,
+} from "./product-card.styles.jsx";
 
 export const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -10,16 +16,19 @@ export const ProductCard = ({ product }) => {
 
   const addProductToCard = () => addItemToCart(product);
   return (
-    <div className="product-card-container">
+    <ProductCardContainer>
       <img src={imageUrl} alt={`${name}`} />
-      <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
-      <Button buttonType="inverted" onClick={addProductToCard}>
+      <ProductFooter>
+        <Name as="span">{name}</Name>
+        <Price>{price}</Price>
+      </ProductFooter>
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
+        onClick={addProductToCard}
+      >
         Add to cart
       </Button>
-    </div>
+    </ProductCardContainer>
   );
 };
 
